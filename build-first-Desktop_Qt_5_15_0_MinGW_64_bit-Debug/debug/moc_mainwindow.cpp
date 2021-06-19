c5677c81dc4d42466ca6177ed8d0077966fca396
Devika RB <devikarb@gmail.com>
2021-06-20 01:56:29 +0530

    Revert "new edited"
    This reverts commit fa50bd2e8eb2049482ddc3d3b825bfccb1f3fac7.

--- build-first-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-first-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../first/mainwindow.h"
+#include "../../../../../Desktop/OOP/group/febi/TIME4CLASS-7da344aeb2a238d03496dc76b00679c2f9f8ff91/first/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[71];
-    char stringdata0[1606];
+    QByteArrayData data[23];
+    char stringdata0[366];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -54,55 +54,7 @@ QT_MOC_LITERAL(18, 243, 22), // "on_MonSub_3_textEdited"
 QT_MOC_LITERAL(19, 266, 23), // "on_MonLink_3_textEdited"
 QT_MOC_LITERAL(20, 290, 28), // "on_MonTime_4_userTimeChanged"
 QT_MOC_LITERAL(21, 319, 22), // "on_MonSub_4_textEdited"
-QT_MOC_LITERAL(22, 342, 23), // "on_MonLink_4_textEdited"
-QT_MOC_LITERAL(23, 366, 29), // "on_TuesTime_1_userTimeChanged"
-QT_MOC_LITERAL(24, 396, 23), // "on_TuesSub_1_textEdited"
-QT_MOC_LITERAL(25, 420, 24), // "on_TuesLink_1_textEdited"
-QT_MOC_LITERAL(26, 445, 29), // "on_TuesTime_2_userTimeChanged"
-QT_MOC_LITERAL(27, 475, 23), // "on_TuesSub_2_textEdited"
-QT_MOC_LITERAL(28, 499, 24), // "on_TuesLink_2_textEdited"
-QT_MOC_LITERAL(29, 524, 29), // "on_TuesTime_3_userTimeChanged"
-QT_MOC_LITERAL(30, 554, 23), // "on_TuesSub_3_textEdited"
-QT_MOC_LITERAL(31, 578, 24), // "on_TuesLink_3_textEdited"
-QT_MOC_LITERAL(32, 603, 29), // "on_TuesTime_4_userTimeChanged"
-QT_MOC_LITERAL(33, 633, 23), // "on_TuesSub_4_textEdited"
-QT_MOC_LITERAL(34, 657, 24), // "on_TuesLink_4_textEdited"
-QT_MOC_LITERAL(35, 682, 28), // "on_WedTime_1_userTimeChanged"
-QT_MOC_LITERAL(36, 711, 22), // "on_WedSub_1_textEdited"
-QT_MOC_LITERAL(37, 734, 23), // "on_WedLink_1_textEdited"
-QT_MOC_LITERAL(38, 758, 28), // "on_WedTime_2_userTimeChanged"
-QT_MOC_LITERAL(39, 787, 22), // "on_WedSub_2_textEdited"
-QT_MOC_LITERAL(40, 810, 23), // "on_WedLink_2_textEdited"
-QT_MOC_LITERAL(41, 834, 28), // "on_WedTime_3_userTimeChanged"
-QT_MOC_LITERAL(42, 863, 22), // "on_WedSub_3_textEdited"
-QT_MOC_LITERAL(43, 886, 23), // "on_WedLink_3_textEdited"
-QT_MOC_LITERAL(44, 910, 28), // "on_WedTime_4_userTimeChanged"
-QT_MOC_LITERAL(45, 939, 22), // "on_WedSub_4_textEdited"
-QT_MOC_LITERAL(46, 962, 23), // "on_WedLink_4_textEdited"
-QT_MOC_LITERAL(47, 986, 29), // "on_ThurTime_1_userTimeChanged"
-QT_MOC_LITERAL(48, 1016, 23), // "on_ThurSub_1_textEdited"
-QT_MOC_LITERAL(49, 1040, 24), // "on_ThurLink_1_textEdited"
-QT_MOC_LITERAL(50, 1065, 29), // "on_ThurTime_2_userTimeChanged"
-QT_MOC_LITERAL(51, 1095, 23), // "on_ThurSub_2_textEdited"
-QT_MOC_LITERAL(52, 1119, 24), // "on_ThurLink_2_textEdited"
-QT_MOC_LITERAL(53, 1144, 29), // "on_ThurTime_3_userTimeChanged"
-QT_MOC_LITERAL(54, 1174, 23), // "on_ThurSub_3_textEdited"
-QT_MOC_LITERAL(55, 1198, 24), // "on_ThurLink_3_textEdited"
-QT_MOC_LITERAL(56, 1223, 29), // "on_ThurTime_4_userTimeChanged"
-QT_MOC_LITERAL(57, 1253, 23), // "on_ThurSub_4_textEdited"
-QT_MOC_LITERAL(58, 1277, 24), // "on_ThurLink_4_textEdited"
-QT_MOC_LITERAL(59, 1302, 28), // "on_FriTime_1_userTimeChanged"
-QT_MOC_LITERAL(60, 1331, 22), // "on_FriSub_1_textEdited"
-QT_MOC_LITERAL(61, 1354, 23), // "on_FriLink_1_textEdited"
-QT_MOC_LITERAL(62, 1378, 28), // "on_FriTime_2_userTimeChanged"
-QT_MOC_LITERAL(63, 1407, 22), // "on_FriSub_2_textEdited"
-QT_MOC_LITERAL(64, 1430, 23), // "on_FriLink_2_textEdited"
-QT_MOC_LITERAL(65, 1454, 28), // "on_FriTime_3_userTimeChanged"
-QT_MOC_LITERAL(66, 1483, 22), // "on_FriSub_3_textEdited"
-QT_MOC_LITERAL(67, 1506, 23), // "on_FriLink_3_textEdited"
-QT_MOC_LITERAL(68, 1530, 28), // "on_FriTime_4_userTimeChanged"
-QT_MOC_LITERAL(69, 1559, 22), // "on_FriSub_4_textEdited"
-QT_MOC_LITERAL(70, 1582, 23) // "on_FriLink_4_textEdited"
+QT_MOC_LITERAL(22, 342, 23) // "on_MonLink_4_textEdited"
 
     },
     "MainWindow\0addTime\0\0Time\0no\0Addlink\0"
@@ -114,47 +66,7 @@ QT_MOC_LITERAL(70, 1582, 23) // "on_FriLink_4_textEdited"
     "on_MonTime_3_userTimeChanged\0"
     "on_MonSub_3_textEdited\0on_MonLink_3_textEdited\0"
     "on_MonTime_4_userTimeChanged\0"
-    "on_MonSub_4_textEdited\0on_MonLink_4_textEdited\0"
-    "on_TuesTime_1_userTimeChanged\0"
-    "on_TuesSub_1_textEdited\0"
-    "on_TuesLink_1_textEdited\0"
-    "on_TuesTime_2_userTimeChanged\0"
-    "on_TuesSub_2_textEdited\0"
-    "on_TuesLink_2_textEdited\0"
-    "on_TuesTime_3_userTimeChanged\0"
-    "on_TuesSub_3_textEdited\0"
-    "on_TuesLink_3_textEdited\0"
-    "on_TuesTime_4_userTimeChanged\0"
-    "on_TuesSub_4_textEdited\0"
-    "on_TuesLink_4_textEdited\0"
-    "on_WedTime_1_userTimeChanged\0"
-    "on_WedSub_1_textEdited\0on_WedLink_1_textEdited\0"
-    "on_WedTime_2_userTimeChanged\0"
-    "on_WedSub_2_textEdited\0on_WedLink_2_textEdited\0"
-    "on_WedTime_3_userTimeChanged\0"
-    "on_WedSub_3_textEdited\0on_WedLink_3_textEdited\0"
-    "on_WedTime_4_userTimeChanged\0"
-    "on_WedSub_4_textEdited\0on_WedLink_4_textEdited\0"
-    "on_ThurTime_1_userTimeChanged\0"
-    "on_ThurSub_1_textEdited\0"
-    "on_ThurLink_1_textEdited\0"
-    "on_ThurTime_2_userTimeChanged\0"
-    "on_ThurSub_2_textEdited\0"
-    "on_ThurLink_2_textEdited\0"
-    "on_ThurTime_3_userTimeChanged\0"
-    "on_ThurSub_3_textEdited\0"
-    "on_ThurLink_3_textEdited\0"
-    "on_ThurTime_4_userTimeChanged\0"
-    "on_ThurSub_4_textEdited\0"
-    "on_ThurLink_4_textEdited\0"
-    "on_FriTime_1_userTimeChanged\0"
-    "on_FriSub_1_textEdited\0on_FriLink_1_textEdited\0"
-    "on_FriTime_2_userTimeChanged\0"
-    "on_FriSub_2_textEdited\0on_FriLink_2_textEdited\0"
-    "on_FriTime_3_userTimeChanged\0"
-    "on_FriSub_3_textEdited\0on_FriLink_3_textEdited\0"
-    "on_FriTime_4_userTimeChanged\0"
-    "on_FriSub_4_textEdited\0on_FriLink_4_textEdited"
+    "on_MonSub_4_textEdited\0on_MonLink_4_textEdited"
 };
 #undef QT_MOC_LITERAL
 
@@ -164,7 +76,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      63,   14, // methods
+      15,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -172,122 +84,26 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    2,  329,    2, 0x08 /* Private */,
-       5,    2,  334,    2, 0x08 /* Private */,
-       7,    2,  339,    2, 0x08 /* Private */,
-       9,    1,  344,    2, 0x08 /* Private */,
-      11,    1,  347,    2, 0x08 /* Private */,
-      13,    1,  350,    2, 0x08 /* Private */,
-      14,    1,  353,    2, 0x08 /* Private */,
-      15,    1,  356,    2, 0x08 /* Private */,
-      16,    1,  359,    2, 0x08 /* Private */,
-      17,    1,  362,    2, 0x08 /* Private */,
-      18,    1,  365,    2, 0x08 /* Private */,
-      19,    1,  368,    2, 0x08 /* Private */,
-      20,    1,  371,    2, 0x08 /* Private */,
-      21,    1,  374,    2, 0x08 /* Private */,
-      22,    1,  377,    2, 0x08 /* Private */,
-      23,    1,  380,    2, 0x08 /* Private */,
-      24,    1,  383,    2, 0x08 /* Private */,
-      25,    1,  386,    2, 0x08 /* Private */,
-      26,    1,  389,    2, 0x08 /* Private */,
-      27,    1,  392,    2, 0x08 /* Private */,
-      28,    1,  395,    2, 0x08 /* Private */,
-      29,    1,  398,    2, 0x08 /* Private */,
-      30,    1,  401,    2, 0x08 /* Private */,
-      31,    1,  404,    2, 0x08 /* Private */,
-      32,    1,  407,    2, 0x08 /* Private */,
-      33,    1,  410,    2, 0x08 /* Private */,
-      34,    1,  413,    2, 0x08 /* Private */,
-      35,    1,  416,    2, 0x08 /* Private */,
-      36,    1,  419,    2, 0x08 /* Private */,
-      37,    1,  422,    2, 0x08 /* Private */,
-      38,    1,  425,    2, 0x08 /* Private */,
-      39,    1,  428,    2, 0x08 /* Private */,
-      40,    1,  431,    2, 0x08 /* Private */,
-      41,    1,  434,    2, 0x08 /* Private */,
-      42,    1,  437,    2, 0x08 /* Private */,
-      43,    1,  440,    2, 0x08 /* Private */,
-      44,    1,  443,    2, 0x08 /* Private */,
-      45,    1,  446,    2, 0x08 /* Private */,
-      46,    1,  449,    2, 0x08 /* Private */,
-      47,    1,  452,    2, 0x08 /* Private */,
-      48,    1,  455,    2, 0x08 /* Private */,
-      49,    1,  458,    2, 0x08 /* Private */,
-      50,    1,  461,    2, 0x08 /* Private */,
-      51,    1,  464,    2, 0x08 /* Private */,
-      52,    1,  467,    2, 0x08 /* Private */,
-      53,    1,  470,    2, 0x08 /* Private */,
-      54,    1,  473,    2, 0x08 /* Private */,
-      55,    1,  476,    2, 0x08 /* Private */,
-      56,    1,  479,    2, 0x08 /* Private */,
-      57,    1,  482,    2, 0x08 /* Private */,
-      58,    1,  485,    2, 0x08 /* Private */,
-      59,    1,  488,    2, 0x08 /* Private */,
-      60,    1,  491,    2, 0x08 /* Private */,
-      61,    1,  494,    2, 0x08 /* Private */,
-      62,    1,  497,    2, 0x08 /* Private */,
-      63,    1,  500,    2, 0x08 /* Private */,
-      64,    1,  503,    2, 0x08 /* Private */,
-      65,    1,  506,    2, 0x08 /* Private */,
-      66,    1,  509,    2, 0x08 /* Private */,
-      67,    1,  512,    2, 0x08 /* Private */,
-      68,    1,  515,    2, 0x08 /* Private */,
-      69,    1,  518,    2, 0x08 /* Private */,
-      70,    1,  521,    2, 0x08 /* Private */,
+       1,    2,   89,    2, 0x08 /* Private */,
+       5,    2,   94,    2, 0x08 /* Private */,
+       7,    2,   99,    2, 0x08 /* Private */,
+       9,    1,  104,    2, 0x08 /* Private */,
+      11,    1,  107,    2, 0x08 /* Private */,
+      13,    1,  110,    2, 0x08 /* Private */,
+      14,    1,  113,    2, 0x08 /* Private */,
+      15,    1,  116,    2, 0x08 /* Private */,
+      16,    1,  119,    2, 0x08 /* Private */,
+      17,    1,  122,    2, 0x08 /* Private */,
+      18,    1,  125,    2, 0x08 /* Private */,
+      19,    1,  128,    2, 0x08 /* Private */,
+      20,    1,  131,    2, 0x08 /* Private */,
+      21,    1,  134,    2, 0x08 /* Private */,
+      22,    1,  137,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QTime, QMetaType::Int,    3,    4,
     QMetaType::Void, QMetaType::QString, QMetaType::Int,    6,    4,
     QMetaType::Void, QMetaType::QString, QMetaType::Int,    8,    4,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QTime,   10,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void, QMetaType::QString,   12,
     QMetaType::Void, QMetaType::QTime,   10,
     QMetaType::Void, QMetaType::QString,   12,
     QMetaType::Void, QMetaType::QString,   12,
@@ -325,54 +141,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 12: _t->on_MonTime_4_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
         case 13: _t->on_MonSub_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 14: _t->on_MonLink_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 15: _t->on_TuesTime_1_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 16: _t->on_TuesSub_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 17: _t->on_TuesLink_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 18: _t->on_TuesTime_2_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 19: _t->on_TuesSub_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 20: _t->on_TuesLink_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 21: _t->on_TuesTime_3_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 22: _t->on_TuesSub_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 23: _t->on_TuesLink_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 24: _t->on_TuesTime_4_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 25: _t->on_TuesSub_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 26: _t->on_TuesLink_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 27: _t->on_WedTime_1_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 28: _t->on_WedSub_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 29: _t->on_WedLink_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 30: _t->on_WedTime_2_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 31: _t->on_WedSub_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 32: _t->on_WedLink_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 33: _t->on_WedTime_3_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 34: _t->on_WedSub_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 35: _t->on_WedLink_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 36: _t->on_WedTime_4_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 37: _t->on_WedSub_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 38: _t->on_WedLink_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 39: _t->on_ThurTime_1_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 40: _t->on_ThurSub_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 41: _t->on_ThurLink_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 42: _t->on_ThurTime_2_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 43: _t->on_ThurSub_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 44: _t->on_ThurLink_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 45: _t->on_ThurTime_3_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 46: _t->on_ThurSub_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 47: _t->on_ThurLink_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 48: _t->on_ThurTime_4_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 49: _t->on_ThurSub_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 50: _t->on_ThurLink_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 51: _t->on_FriTime_1_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 52: _t->on_FriSub_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 53: _t->on_FriLink_1_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 54: _t->on_FriTime_2_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 55: _t->on_FriSub_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 56: _t->on_FriLink_2_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 57: _t->on_FriTime_3_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 58: _t->on_FriSub_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 59: _t->on_FriLink_3_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 60: _t->on_FriTime_4_userTimeChanged((*reinterpret_cast< const QTime(*)>(_a[1]))); break;
-        case 61: _t->on_FriSub_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 62: _t->on_FriLink_4_textEdited((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -407,13 +175,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 63)
+        if (_id < 15)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 63;
+        _id -= 15;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 63)
+        if (_id < 15)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 63;
+        _id -= 15;
     }
     return _id;
 }
